efca36173bef51cb31bcb5365a9dadbbe58fee04
lilin <lilin@bailongma-inc.com>
2025-03-08 08:56:04 +0800

    feat: 链表

---
+++ leetcode/listNode/82. 删除排序链表中的重复元素 II.js
@@ -0,0 +1,19 @@
+/**
+ * @description: 82. 删除排序链表中的重复元素 II
+ * 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {};
---
+++ leetcode/listNode/83. 删除排序链表中的重复元素.js
@@ -0,0 +1,19 @@
+/**
+ * @description: 83. 删除排序链表中的重复元素
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {};
